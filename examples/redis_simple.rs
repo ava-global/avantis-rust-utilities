63762b196265b7dd12e31dc779f78a441e58befb
Tawan 'catflyflyfly' Wattana <chittawan.w@ava.fund>
2022-05-25 18:38:49 +0700

    chore: strictly separate module by feature flags
    tawan

--- examples/redis_simple.rs
+++ examples/redis_simple.rs
@@ -1,78 +1,99 @@
-use std::time::Duration;
-
-use ::redis::AsyncCommands;
 use anyhow::Result;
-use avantis_utils::config::load_config;
-use avantis_utils::config::Environment;
-use avantis_utils::redis::Connection;
-use avantis_utils::redis::GetOrRefreshExt;
-use avantis_utils::redis::Pool;
-use avantis_utils::redis::RedisConfig;
-use once_cell::sync::Lazy;
 use tokio;
-use tokio::sync::OnceCell;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut redis = get_redis_connection().await?;
-
-    let _: () = redis.set("name", "John").await?;
-    let name: String = redis.get("name").await?;
-    let _: () = redis.del("name").await?;
-
-    println!("name: {name}");
-
-    get_redis_connection()
-        .await?
-        .get_or_refresh("text", || async { hello_world(0).await }, 1)
-        .await?;
-    let text: String = get_redis_connection()
-        .await?
-        .get_or_refresh("text", || async { hello_world(1).await }, 1)
-        .await?;
-
-    println!("text: {text}");
-
-    tokio::time::sleep(Duration::from_secs(1)).await;
-
-    get_redis_connection()
-        .await?
-        .get_or_refresh("text", || async { hello_world(2).await }, 1)
-        .await?;
-    let text: String = get_redis_connection()
-        .await?
-        .get_or_refresh("text", || async { hello_world(3).await }, 1)
-        .await?;
-
-    println!("text: {text}");
-
-    Ok(())
+    inner::main().await
 }
 
-async fn hello_world(time: usize) -> anyhow::Result<String> {
-    Ok(format!("Hello World: {time}"))
+#[cfg(all(feature = "cfg", feature = "redis-utils"))]
+mod inner {
+    use super::*;
+
+    use std::time::Duration;
+
+    use ::redis::AsyncCommands;
+    use avantis_utils::config::load_config;
+    use avantis_utils::config::Environment;
+    use avantis_utils::redis::Connection;
+    use avantis_utils::redis::GetOrRefreshExt;
+    use avantis_utils::redis::Pool;
+    use avantis_utils::redis::RedisConfig;
+    use once_cell::sync::Lazy;
+    use tokio::sync::OnceCell;
+
+    pub async fn main() -> Result<()> {
+        let mut redis = get_redis_connection().await?;
+
+        let _: () = redis.set("name", "John").await?;
+        let name: String = redis.get("name").await?;
+        let _: () = redis.del("name").await?;
+
+        println!("name: {name}");
+
+        get_redis_connection()
+            .await?
+            .get_or_refresh("text", || async { hello_world(0).await }, 1)
+            .await?;
+        let text: String = get_redis_connection()
+            .await?
+            .get_or_refresh("text", || async { hello_world(1).await }, 1)
+            .await?;
+
+        println!("text: {text}");
+
+        tokio::time::sleep(Duration::from_secs(1)).await;
+
+        get_redis_connection()
+            .await?
+            .get_or_refresh("text", || async { hello_world(2).await }, 1)
+            .await?;
+        let text: String = get_redis_connection()
+            .await?
+            .get_or_refresh("text", || async { hello_world(3).await }, 1)
+            .await?;
+
+        println!("text: {text}");
+
+        Ok(())
+    }
+
+    async fn hello_world(time: usize) -> anyhow::Result<String> {
+        Ok(format!("Hello World: {time}"))
+    }
+
+    static CONFIG: Lazy<ExampleConfig> =
+        Lazy::new(|| ExampleConfig::load(Environment::Develop).unwrap());
+
+    static REDIS_POOL: OnceCell<Pool> = OnceCell::const_new();
+    pub(crate) async fn get_redis_connection() -> anyhow::Result<Connection> {
+        REDIS_POOL
+            .get_or_init(|| async { CONFIG.redis.init_pool().await.unwrap() })
+            .await
+            .get()
+            .await
+            .map_err(|err| err.into())
+    }
+
+    #[derive(Clone, Debug, PartialEq, serde::Deserialize)]
+    struct ExampleConfig {
+        redis: RedisConfig,
+    }
+
+    impl ExampleConfig {
+        fn load(environment: Environment) -> anyhow::Result<Self> {
+            load_config(environment)
+        }
+    }
 }
 
-static CONFIG: Lazy<ExampleConfig> =
-    Lazy::new(|| ExampleConfig::load(Environment::Develop).unwrap());
+#[cfg(not(all(feature = "cfg", feature = "redis-utils")))]
+mod inner {
+    use super::*;
 
-static REDIS_POOL: OnceCell<Pool> = OnceCell::const_new();
-pub(crate) async fn get_redis_connection() -> anyhow::Result<Connection> {
-    REDIS_POOL
-        .get_or_init(|| async { CONFIG.redis.init_pool().await.unwrap() })
-        .await
-        .get()
-        .await
-        .map_err(|err| err.into())
-}
+    pub async fn main() -> Result<()> {
+        println!("Please pass --features cfg,redis-utils to cargo when trying this example.");
 
-#[derive(Clone, Debug, PartialEq, serde::Deserialize)]
-struct ExampleConfig {
-    redis: RedisConfig,
-}
-
-impl ExampleConfig {
-    fn load(environment: Environment) -> anyhow::Result<Self> {
-        load_config(environment)
+        Ok(())
     }
 }
