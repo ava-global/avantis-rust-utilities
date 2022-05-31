49b2b570e67c137d6aac661c532c87b98fa43894
Veha Suwatphisankij <vehas@users.noreply.github.com>
2022-05-31 10:36:31 +0700

    change kafka feature name

--- examples/kafka_simple.rs
+++ examples/kafka_simple.rs
@@ -6,15 +6,16 @@ async fn main() -> Result<()> {
     inner::main().await
 }
 
-#[cfg(all(feature = "cfg", feature = "kafka-utils"))]
+#[cfg(all(feature = "cfg", feature = "kafka"))]
 mod inner {
     use super::*;
     pub async fn main() -> Result<()> {
         println!("use kafka utils");
+        Ok(())
     }
 }
 
-#[cfg(not(all(feature = "cfg", feature = "kafka-utils")))]
+#[cfg(not(all(feature = "cfg", feature = "kafka")))]
 mod inner {
     use super::*;
 
