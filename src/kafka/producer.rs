d42ccd00be8b001c5365951c6373b3b14bff1122
Tawan 'catflyflyfly' Wattana <chittawan.w@ava.fund>
2022-06-02 17:45:28 +0700

    feat: simplify consumer & producer to use FutureProducer & StreamConsumer
    tawan, veha

--- src/kafka/producer.rs
+++ src/kafka/producer.rs
@@ -1,108 +1,25 @@
-use std::fmt::{Debug, Formatter};
-
-use std::time::Duration;
-
-use super::KafkaConfig;
-use itertools::{Either, Itertools};
-
-use rdkafka::producer::{BaseProducer, BaseRecord, Producer};
+use rdkafka::config::FromClientConfig;
 use rdkafka::ClientConfig;
 
-use super::KafkaKeyMessagePair;
+use super::KafkaConfig;
 
-pub struct KafkaProducerImpl {
-    pub producer: BaseProducer,
-    pub setting: KafkaConfig,
-}
+pub use rdkafka::producer::{FutureProducer, FutureRecord};
+pub use rdkafka::util::Timeout;
 
-impl Debug for KafkaProducerImpl {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("KafkaProducerImpl").finish()
-    }
-}
-
-pub trait KafkaProducer {
-    fn send(&self, topic_name: String, bytes_message: KafkaKeyMessagePair);
-    fn send_and_flush(&self, topic_name: String, bytes_message: KafkaKeyMessagePair);
-    fn flush(&self);
-    fn bulk_send_and_flush(
-        &self,
-        topic_name: String,
-        bytes_messages: &[KafkaKeyMessagePair],
-    ) -> anyhow::Result<()>;
-}
-
-impl KafkaProducer for KafkaProducerImpl {
-    fn send(&self, topic_name: String, bytes_message: KafkaKeyMessagePair) {
-        let record = BaseRecord::to(topic_name.as_str())
-            .payload(&bytes_message.message.value)
-            .key(&bytes_message.key);
-        self.producer
-            .send(record)
-            .unwrap_or_else(|_| panic!("Cannot produce message to {}", topic_name));
-
-        self.producer
-            .poll(Duration::from_millis(self.setting.poll_duration_millis));
-    }
-
-    fn flush(&self) {
-        self.producer
-            .flush(Duration::from_millis(self.setting.flush_duration_millis));
-    }
-    fn send_and_flush(&self, topic_name: String, bytes_message: KafkaKeyMessagePair) {
-        self.send(topic_name, bytes_message);
-        self.flush();
-    }
-
-    fn bulk_send_and_flush(
-        &self,
-        topic_name: String,
-        bytes_messages: &[KafkaKeyMessagePair],
-    ) -> anyhow::Result<()> {
-        let r1 = bytes_messages.iter().map(|data| {
-            let record = BaseRecord::to(topic_name.as_str())
-                .key(&data.key)
-                .payload(&data.message.value);
-
-            self.producer.send(record)
-        });
-
-        let (_, failure): (Vec<_>, Vec<_>) = r1.partition_map(|r| match r {
-            Ok(a) => Either::Left(a),
-            Err(b) => Either::Right(b),
-        });
-
-        self.producer
-            .flush(Duration::from_millis(self.setting.flush_duration_millis));
-
-        if failure.is_empty() {
-            Ok(())
-        } else {
-            anyhow::bail!(format!("{:?}", failure))
-        }
-    }
-}
-
-impl KafkaProducerImpl {
-    pub fn new(kafka_setting: KafkaConfig) -> Self {
-        let producer = ClientConfig::new()
-            .set("bootstrap.servers", kafka_setting.brokers().join(","))
-            .set("queue.buffering.max.messages", "1000000")
-            .set("queue.buffering.max.ms", "5")
+impl KafkaConfig {
+    pub fn producer_config<T: FromClientConfig>(&self) -> T {
+        ClientConfig::new()
+            .set("bootstrap.servers", &self.brokers_csv)
+            .set("message.timeout.ms", "30000")
             .set(
                 "security.protocol",
-                kafka_setting
-                    .security_protocol
+                self.security_protocol
                     .clone()
                     .unwrap_or_else(|| "ssl".to_string()),
             )
-            .set("log.connection.close", "false")
+            .set_log_level(rdkafka::config::RDKafkaLogLevel::Debug)
+            // .set("log.connection.close", "false")
             .create()
-            .expect("Producer creation error");
-
-        Self {
-            producer,
-            setting: kafka_setting,
-        }
+            .expect("Producer creation error")
     }
 }
